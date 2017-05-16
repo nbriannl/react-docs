de1bc327b8d9fdc5c033d1144db1a731733237a5
nbriannl <neilbrian.nl@gmail.com>
2017-05-16 17:16:41 +0800

    commit to master
    so tiring. i need to git gud

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
+
+function formatName(user) {
+  return user.firstName + ' ' + user.lastName;
+}
+
+//test comment
+
+const user = {
+  firstName: 'Harper',
+  lastName: 'Perez'
+};
+
+const element = (
+  <a href="https://www.google.com"><h1>
+    {/*You can wrap JavaScript expressions inside JSX */}
+    Hello, {formatName(user)}!
+  </h1></a>
+);
+
+ReactDOM.render(
+  element,
+  document.getElementById('root')
+);
