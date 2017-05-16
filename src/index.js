42b44573e043b8ca31126938a6545f091f7a21b6
nbriannl <neilbrian.nl@gmail.com>
2017-05-16 19:09:08 +0800

    a4
    components

--- src/index.js
+++ src/index.js
@@ -3,21 +3,81 @@ import ReactDOM from 'react-dom';
 //import App from './App';
 import './index.css';
 
-function Welcome(props) {
-  return <h1>Hello, {props.name}</h1>;
+function formatDate(date) {
+  return date.toLocaleDateString();
 }
 
-function App() {
+/*
+function Comment(props) {
   return (
-    <div>
-      <Welcome name="Sara" />
-      <Welcome name="Cahal" />
-      <Welcome name="Edite" />
+    <div className="Comment">
+      <div className="UserInfo">
+        <img className="Avatar"
+             src={props.author.avatarUrl}
+             alt={props.author.name} />
+        <div className="UserInfo-name">
+          {props.author.name}
+        </div>
+      </div>
+      <div className="Comment-text">
+        {props.text}
+      </div>
+      <div className="Comment-date">
+        {formatDate(props.date)}
+      </div>
+    </div>
+  );
+}
+*/
+
+function Comment(props) {
+  return (
+    <div className="Comment">
+      <UserInfo user={props.author} />
+      <div className="Comment-text">
+        {props.text}
+      </div>
+      <div className="Comment-date">
+        {formatDate(props.date)}
+      </div>
     </div>
   );
 }
 
+//name props from the components own POV as opposed to the context of its usage.
+function Avatar(props) {
+  return (
+    <img className="Avatar"
+      src={props.user.avatarUrl}
+      alt={props.user.name}
+    />
+  )
+}
+
+function UserInfo(props) {
+  return (
+    <div className="UserInfo">
+      <Avatar user={props.user} />
+      <div className="UserInfo-name">
+        {props.user.name}
+      </div>
+    </div>
+  );
+}
+
+
+const comment = {
+  date: new Date(),
+  text: 'I hope you enjoy learning React!',
+  author: {
+    name: 'Hello Kitty',
+    avatarUrl: 'http://placekitten.com/g/64/64'
+  }
+};
 ReactDOM.render(
-  <App />,
+  <Comment
+    date={comment.date}
+    text={comment.text}
+    author={comment.author} />,
   document.getElementById('root')
 );
