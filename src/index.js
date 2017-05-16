616ae007c4add0a565c24226d2f594dfe8045a09
nbriannl <neilbrian.nl@gmail.com>
2017-05-16 18:11:35 +0800

    a3

--- src/index.js
+++ src/index.js
@@ -1,40 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+//import App from './App';
 import './index.css';
 
-function formatName(user) {
-  return user.firstName + ' ' + user.lastName;
+function Welcome(props) {
+  return <h1>Hello, {props.name}</h1>;
 }
 
-
-const user = {
-  firstName: 'Jarper',
-  lastName: 'Perez'
-};
-
-const name = (
-  <h2>{getGreeting({firstName: "carrot", lastName: "tomato"})}</h2>
-);
-
-function getGreeting(user) {
-  if (user) {
-    return <h1>Hello, {formatName(user)}!</h1>;
-  }
-  return <h1>Hello, Stranger.</h1>;
-}
-
-function tick() {
-  const element = (
+function App() {
+  return (
     <div>
-      <h1>{name}</h1>
-      <h2>It is {new Date().toLocaleTimeString()}.</h2>
+      <Welcome name="Sara" />
+      <Welcome name="Cahal" />
+      <Welcome name="Edite" />
     </div>
   );
-  ReactDOM.render(
-    element,
-    document.getElementById('root')
-  );
 }
 
-setInterval(tick, 1000);
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
