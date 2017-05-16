7d67080b87b28f03e4815490a3afa3534f2e3ab7
nbriannl <neilbrian.nl@gmail.com>
2017-05-16 17:35:43 +0800

    a1

--- src/index.js
+++ src/index.js
@@ -9,16 +9,30 @@ function formatName(user) {
 
 
 const user = {
-  firstName: 'Harper',
+  firstName: 'Jarper',
   lastName: 'Perez'
 };
 
+{/*You can wrap JavaScript expressions inside JSX */}
+/*
 const element = (
   <a href="https://www.google.com"><h1>
-    {/*You can wrap JavaScript expressions inside JSX */}
     Hello, {formatName(user)}!
   </h1></a>
 );
+*/
+
+const element = (
+  <h1>{getGreeting({firstName: "carrot", lastName: "tomato"})}</h1>
+);
+
+
+function getGreeting(user) {
+  if (user) {
+    return <h1>Hello, {formatName(user)}!</h1>;
+  }
+  return <h1>Hello, Stranger.</h1>;
+}
 
 ReactDOM.render(
   element,
