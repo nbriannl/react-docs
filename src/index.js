134b1413982ea1d76a637aedd5d8a97ad19c5d5e
nbriannl <neilbrian.nl@gmail.com>
2017-05-17 11:55:36 +0800

    a5
    Added Comments to Clock!

--- src/index.js
+++ src/index.js
@@ -3,81 +3,72 @@ import ReactDOM from 'react-dom';
 //import App from './App';
 import './index.css';
 
-function formatDate(date) {
-  return date.toLocaleDateString();
-}
-
 /*
-function Comment(props) {
-  return (
-    <div className="Comment">
-      <div className="UserInfo">
-        <img className="Avatar"
-             src={props.author.avatarUrl}
-             alt={props.author.name} />
-        <div className="UserInfo-name">
-          {props.author.name}
-        </div>
-      </div>
-      <div className="Comment-text">
-        {props.text}
-      </div>
-      <div className="Comment-date">
-        {formatDate(props.date)}
-      </div>
+function tick() {
+  const element = (
+    <div>
+      <h1>Hello, world!</h1>
+      <h2>It is {new Date().toLocaleTimeString()}.</h2>
     </div>
+  );
+  ReactDOM.render(
+    element,
+    document.getElementById('root')
   );
 }
 */
 
-function Comment(props) {
-  return (
-    <div className="Comment">
-      <UserInfo user={props.author} />
-      <div className="Comment-text">
-        {props.text}
-      </div>
-      <div className="Comment-date">
-        {formatDate(props.date)}
-      </div>
-    </div>
-  );
-}
-
-//name props from the components own POV as opposed to the context of its usage.
-function Avatar(props) {
-  return (
-    <img className="Avatar"
-      src={props.user.avatarUrl}
-      alt={props.user.name}
-    />
-  )
-}
-
-function UserInfo(props) {
-  return (
-    <div className="UserInfo">
-      <Avatar user={props.user} />
-      <div className="UserInfo-name">
-        {props.user.name}
-      </div>
-    </div>
-  );
-}
-
-
-const comment = {
-  date: new Date(),
-  text: 'I hope you enjoy learning React!',
-  author: {
-    name: 'Hello Kitty',
-    avatarUrl: 'http://placekitten.com/g/64/64'
+/*function Clock(props) {*/
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { date: new Date() };
   }
-};
+
+  //life cycle hooks
+  componentDidMount() {
+    // setInterval() calls a function at the specified interval (in ms)
+    // setInterval( f(){ ... }, interval)
+    this.timerID = setInterval(
+      () => this.tick(), //refer to line 41
+      1000
+    );
+  }
+
+  //more life cycle hooks
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+    this.setState({
+      date: new Date()
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hello, world!</h1>
+        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+      </div>
+    );
+  }
+}
+
+/*function tick() {*/
+/*const element = (
+  <div>
+    <h1>Hello, world!</h1>
+    <h2>It is {new Date().toLocaleTimeString()}.</h2>
+  </div>
+);*/
 ReactDOM.render(
-  <Comment
-    date={comment.date}
-    text={comment.text}
-    author={comment.author} />,
+  /*element,*/
+  /*<Clock date={new Date()} />,*/
+  <Clock />,
   document.getElementById('root')
 );
+/*}*/
+
+/*setInterval(tick, 1000);*/
