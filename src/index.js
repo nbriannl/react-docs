3c56d16a88b334eb3c996bebbece9bd2d19491de
nbriannl <neilbrian.nl@gmail.com>
2017-05-18 13:01:23 +0800

    Before I experiment more

--- src/index.js
+++ src/index.js
@@ -7,33 +7,6 @@ function FormattedDate(props) {
   return <h2>It is {props.date.toLocaleTimeString()}.</h2>;
 }
 
-class Toggle extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { isToggleOn: true };
-
-    // This binding is necessarry to make 'this' work in the callback
-    this.handleClick = this.handleClick.bind(this); 
-  }
-
-  handleClick(){
-    this.setState(prevState => ({
-      isToggleOn: !prevState.isToggleOn
-    }));
-  } 
-
-  render() {
-    return (
-      <button onClick={this.handleClick}>
-        {this.state.isToggleOn ? 'ON' : 'OFF'}
-      </button>
-    );
-  }
-}
-
-
-
-/*function Clock(props) {*/
 class Clock extends React.Component {
   constructor(props) {
     super(props);
@@ -45,7 +18,7 @@ class Clock extends React.Component {
     // setInterval() calls a function at the specified interval (in ms)
     // setInterval( f(){ ... }, interval)
     this.timerID = setInterval(
-      () => this.tick(), //refer to line 41
+      () => this.tick(), //refer to tick()
       1000
     );
   }
@@ -72,30 +45,104 @@ class Clock extends React.Component {
   }
 }
 
+//============================
+
+class Toggle extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { isToggleOn: true };
+
+    // This binding is necessarry to make 'this' work in the callback
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState(prevState => ({
+      isToggleOn: !prevState.isToggleOn
+    }));
+  }
+
+  render() {
+    return (
+      <button onClick={this.handleClick}>
+        {this.state.isToggleOn ? 'ON' : 'OFF'}
+      </button>
+    );
+  }
+}
+
+//============================
+
+class Button extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isToggleOn: true,
+      numPressed: 0
+    };
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState(
+      prevState => {
+        return {
+          isToggleOn: !prevState.isToggleOn,
+          numPressed: prevState.numPressed + 1
+        };
+      }
+    );
+  }
+
+  render() {
+    return (
+      <button onClick={this.handleClick}>
+        <p><b>Number of Times Pressed:</b></p>
+        <p>{this.state.numPressed} {this.state.isToggleOn ? 'ON' : 'OFF'}</p>
+      </button>
+    )
+  }
+}
+
+//==========================
+
+function UserGreeting(props) {
+  return <h1>Welcome back!</h1>;
+}
+
+function GuestGreeting(props) {
+  return (
+    <div>
+      <h1>Please sign up.</h1>
+      <h1>You're a Guest.</h1>
+    </div>
+  );
+}
+
+function Greeting(props) {
+  const isLoggedIn = props.isLoggedIn;
+  if (isLoggedIn) {
+    return <UserGreeting />;
+  }
+  return <GuestGreeting />;
+}
+
+
 function App() {
   return (
     <div>
       <Clock />
       <Clock />
+      <Button />
       <Toggle />
+      <Greeting isLoggedIn={false} />
     </div>
   );
 }
 
 
-/*function tick() {*/
-/*const element = (
-  <div>
-    <h1>Hello, world!</h1>
-    <h2>It is {new Date().toLocaleTimeString()}.</h2>
-  </div>
-);*/
 ReactDOM.render(
-  /*element,*/
-  /*<Clock date={new Date()} />,*/
   <App />,
   document.getElementById('root')
 );
-/*}*/
-
-/*setInterval(tick, 1000);*/
